5f872cf979f613db3a00543ae8ac56bd563f6c83
Aya Alaa <aya-28-10@hotmail.com>
2026-02-02 13:36:21 +0200

    push api file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-const Home = () => {
+import {getMovies} from "./api/moviesAPI.tsx"
+
+const Home = async() => {
+  const data = await getMovies();
+
+  console.log(data)
   return (
     <div>
       <p>Hello World</p>
